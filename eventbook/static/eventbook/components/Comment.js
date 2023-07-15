5e1f25fb8d4f29ac28d14ccb8917cf5af436e66b
kimsoyol <naywinlhaing@gmail.com>
2023-07-15 12:56:16 +0700

    remove edit cmt

--- eventbook/static/eventbook/components/Comment.js
+++ eventbook/static/eventbook/components/Comment.js
@@ -1,32 +1,25 @@
-function EditBtn() {
-  return (
-    <a href="#" className="link-muted position-absolute top-0 end-0">
-      <i className="bi bi-pencil" style={{ color: "black" }}></i>
-    </a>
-  );
-}
 
 function CommentComponent() {
+  // Get event's id to show comments
   const eventId = document
     .querySelector(".cmt-component")
     .getAttribute("data-event-id");
-  const user = document
-    .querySelector(".cmt-component")
-    .getAttribute("data-user");
 
   const [cmts, setCmts] = React.useState([]);
   const [newCmt, setNewCmt] = React.useState("");
 
   React.useEffect(() => {
+    // load all comments 
     fetch(`/comment/${eventId}`)
       .then((response) => response.json())
       .then((comments) => {
         setCmts(comments);
-      });
-  }, [newCmt]);
+      }); 
+  }, [newCmt]); // Reload when new comment added
 
   const addCmt = (e) => {
-    if (e.key === "Enter") {
+    // Add new comment when press enter
+    if (e.key === "Enter" && newCmt !== '') {
       fetch(`/comment/${eventId}`, {
         method: "POST",
         body: JSON.stringify({
@@ -37,7 +30,6 @@ function CommentComponent() {
         .then((comment) => {
           // Append the new comment to the cmts
           setCmts((cmts) => [comment, ...cmts]);
-
           // Clear the input field
           setNewCmt("");
         });
@@ -58,7 +50,7 @@ function CommentComponent() {
       </div>
       {cmts.map((cmt) => (
         <React.Fragment>
-        <div className="card-body p-4">
+        <div className="card-body p-4 ">
           <h6 className="fw-bold mb-1"
             style={{ "text-transform": "capitalize" }}
           >
@@ -68,13 +60,10 @@ function CommentComponent() {
             <p className="mb-0 text-muted">
               <small>{cmt.comment_date}</small>
             </p>
-            {/* Show edit btn if user's comment */}
-            {cmt.user == user ? <EditBtn /> : <div></div>}
           </div>
-          <p className="mb-0" id={cmt.id}>
+          <p className="comment-content mb-0" id={cmt.id}>
             {cmt.content}
-          </p>
-         
+          </p> 
         </div>
         <hr className="my-0" />
         </React.Fragment>
