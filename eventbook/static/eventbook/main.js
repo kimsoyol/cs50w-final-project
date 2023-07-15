b843c82c7898a4859af492a1e14df8e33e6878b1
kimsoyol <naywinlhaing@gmail.com>
2023-07-15 13:34:33 +0700

    fix nav style

--- eventbook/static/eventbook/main.js
+++ eventbook/static/eventbook/main.js
@@ -6,9 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const handleSideBar = () => {
   const x = document.querySelector(".sidebar")
+  const content = document.querySelector(".content-main")
   if (x.style.display === "none") {
     x.style.display = "block";
+    content.style.paddingLeft = '8rem';
   } else {
     x.style.display = "none";
+    content.style.paddingLeft = '0';
   }
 };
